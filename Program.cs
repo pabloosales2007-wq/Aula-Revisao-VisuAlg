600bd02cb7bfe9bc3d56d292161212d7c68c3a29
cysgabrielsz <gabcandido09@gmail.com>
2026-03-13 15:17:28 -0300

    programa do pablo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace testepablo
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("pablo tem medo de inseto? ");
+            string resposta = Console.ReadLine().ToLower();
+
+            if (resposta == "sim")
+            {
+                Console.WriteLine("pablo é frango");
+            }
+            else
+                Console.WriteLine("boa pablo");
+        }
+    }
+}
